381037f577d4467e5f078bbd90d8cf3fbd43c27f
Andy Hull <andy@hull.io>
2019-11-04 11:56:22 -0500

    connector url

--- packages/connectors/hull-zapier/zapier-app/src/authentication/index.js
+++ packages/connectors/hull-zapier/zapier-app/src/authentication/index.js
@@ -2,6 +2,6 @@ module.exports = {
   type: "custom",
   fields: [{ key: "token", label: "Token", required: true, type: "string" }],
   test: {
-    url: "https://hull-staging-zapier.ngrok.io/auth"
+    url: `${process.env.CONNECTOR_URL}/auth`
   }
 };
